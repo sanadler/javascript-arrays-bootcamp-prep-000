16b1c34490d1a790879b3ae46dd388cd448f6146
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-07 21:02:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  [element, ...array];
+  array.unshift(element);
   return array;
 }
