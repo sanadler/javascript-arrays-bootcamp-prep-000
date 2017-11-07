df01fd026a36959670485fe821ffe974257680f4
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-07 20:50:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,9 +2,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
 function addElementToBeginningOfArray(array, elementToAdd){
-  
+  var myArray = new Array()
+  [elementToAdd, ...myArray]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, elementToAdd){
-  
+
 }
